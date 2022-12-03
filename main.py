53910cc0d880319b9d8e590d961c23e744400b12
Vikranth12 <56485625+Vikranth12@users.noreply.github.com>
2022-12-03 14:25:14 +0530

    main edit

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-if __name__=="__main__":
-    pass
+from Functions import PexXorAlgorithm
+from Functions import HexDecimal
+from Functions import bit_string
+from Functions import Inverse_Per_XOR
+
+obj1 = PexXorAlgorithm()
+ValueInBits = obj1.Encryption("T")
+print(ValueInBits)
+
+obj2 = bit_string(ValueInBits)
+ValueInCipher = obj2.bit_StringConversion()
+print(ValueInCipher)
+
+obj3=Inverse_Per_XOR()
+ValueInCiPherBits = obj3.Encryption(ValueInCipher)
+print(ValueInCiPherBits)
+
+obj4 = bit_string(ValueInCiPherBits)
+ValueInCipher = obj4.bit_StringConversion()
+print(ValueInCipher)
